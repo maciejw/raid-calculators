3092a42f0c5ad523c8671df24147bc72c840fb55
Maciej Warszawski <maciej+github@warszawski.pro>
2020-06-03 06:14:56 +0200

    deploy 1.0

---
+++ precache-manifest.cb7a1287794c83df7ce0b5f9aec5a7bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1b97d44065b6ee25b1e6c01a639b13",
+    "url": "/raid-calculators/index.html"
+  },
+  {
+    "revision": "55d5379f27922149d25a",
+    "url": "/raid-calculators/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "b5a85ff207820d2f05ae",
+    "url": "/raid-calculators/static/css/main.c4122063.chunk.css"
+  },
+  {
+    "revision": "55d5379f27922149d25a",
+    "url": "/raid-calculators/static/js/2.fa708d64.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/raid-calculators/static/js/2.fa708d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a85ff207820d2f05ae",
+    "url": "/raid-calculators/static/js/main.4ee610e1.chunk.js"
+  },
+  {
+    "revision": "e6df657ac4680c9094ef",
+    "url": "/raid-calculators/static/js/runtime-main.48035303.js"
+  }
+]);
